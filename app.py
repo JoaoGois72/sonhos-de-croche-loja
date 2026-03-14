ddbe38f9b49cc43d0870afd3fd2827a538bde557
Joao Gois <engjoaomarcelo@outlook.com>
2026-03-14 08:54:02 -0300

    fix cloudinary env variables

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask import Flask, render_template, request, redirect, url_for, flash, abo
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from dotenv import load_dotenv
 import cloudinary
 import cloudinary.uploader
 
 
+load_dotenv()
 
 db = SQLAlchemy()
 login_manager = LoginManager()
